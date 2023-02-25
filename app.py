bd4bd7c073af4915c14094275e9c5edcc5f46ebd
Taciana3090 <taciana3090@gmail.com>
2023-02-24 21:46:57 -0300

    Alterando o nome da pasta

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import streamlit as st
 
 
 # importando os dados
-dados = pd.read_csv('C:\dev\Streamlit-CAGED\data\CAGED-PE_LIMPO.csv')
+df = pd.read_csv('C:\dev\Streamlit-CAGED\Data\CAGED-PE_LIMPO.csv')
 
 # Centraliza o título utilizando HTML
 st.write("<h1 style='text-align: center;'>Dashboard do CAGED-PE</h1>", unsafe_allow_html=True)
+
 
 
 
